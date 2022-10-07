056e80ff2a75cb4227a69e19752e5158dabefd33
Haisheng W - M <haisheng@mercury.com>
2022-10-06 22:45:44 -0700

    POC - adding log example

--- Controllers/FibonacciController.cs
+++ Controllers/FibonacciController.cs
@@ -1,9 +1,13 @@
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Trace;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
 
 [Route("/fib")]
 [ApiController]
@@ -12,19 +16,23 @@ public class FibonacciController : ControllerBase
     private readonly Tracer _tracer;
     private readonly IHttpClientFactory _clientFactory;
     private readonly string _projectDomain = Environment.GetEnvironmentVariable("PROJECT_DOMAIN");
+    private readonly ILogger _logger;
 
-    public FibonacciController(Tracer tracer, IHttpClientFactory clientFactory)
+    public FibonacciController(Tracer tracer, IHttpClientFactory clientFactory, ILogger<FibonacciController> logger)
     {
         _tracer = tracer;
         _clientFactory = clientFactory;
+        _logger = logger;
     }
 
     [HttpGet]
     public async Task<int> CalculateFibonacciAsync(int index = 0)
     {
+        _logger.LogInformation("start calculate fib {index}.", index);
+
         // CUSTOM ATTRIBUTES (2 lines of code to uncomment)
-        // var currentSpan = Tracer.CurrentSpan;
-        // currentSpan.SetAttribute("parameter.index", index);
+        var currentSpan = Tracer.CurrentSpan;
+        currentSpan.SetAttribute("parameter.index", index);
 
         if (index == 0 | index == 1)
             return 0;
@@ -35,12 +43,13 @@ public class FibonacciController : ControllerBase
         var resTwo = await GetNext(index - 2);
 
         // CUSTOM SPAN (3 sections of code to uncomment, 4 lines total)
-        // using (var span = _tracer.StartActiveSpan("calculation"))
-        // {
-        var fibonacciNumber = resOne + resTwo;
-        //    span.SetAttribute("result", fibonacciNumber);
-        return fibonacciNumber;
-        // };
+        using (var span = _tracer.StartActiveSpan("calculation"))
+        {
+           var fibonacciNumber = resOne + resTwo;
+           span.SetAttribute("result", fibonacciNumber);
+           _logger.LogInformation("done calculate fib {result}.", fibonacciNumber);
+           return fibonacciNumber;
+        };
     }
 
     private async Task<int> GetNext(int iv)
--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
 
 namespace intro_to_observability_dotnet
 {
@@ -12,6 +24,25 @@ namespace intro_to_observability_dotnet
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                 .ConfigureLogging(builder =>
+                 {
+                     var appResourceBuilder = ResourceBuilder.CreateDefault()
+                         .AddService("fib-microsvc-log", "0.0.1");
+
+                     builder.AddOpenTelemetry(options =>
+                     {
+                         options.SetResourceBuilder(appResourceBuilder);
+                         options.AddOtlpExporter(option =>
+                         {
+                             option.Endpoint = new Uri("https://api.honeycomb.io");
+                             option.Headers = "x-honeycomb-team=xxxxx";
+                         });
+                     });
+
+                     // logBuilder.ClearProviders(); // removes all providers from LoggerFactory
+                     // logBuilder.AddConsole();
+                     // logBuilder.AddTraceSource("Information, ActivityTracing"); // Add Trace listener provider
+                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
